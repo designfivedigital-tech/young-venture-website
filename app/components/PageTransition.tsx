9587b194512b64551af1d8811e7adf4cfe3914c3
Marcos <marcos.baldanzi@gmail.com>
2026-06-16 12:00:04 +0200

    Update website

--- app/components/PageTransition.tsx
+++ app/components/PageTransition.tsx
@@ -10,7 +10,7 @@ export default function PageTransition({ children }: { children: ReactNode }) {
 
   return (
     <div className="page-transition-root">
-      <AnimatePresence mode="wait">
+      <AnimatePresence initial={false}>
         <motion.div
           key={pathname}
           className="page-slide-motion"
